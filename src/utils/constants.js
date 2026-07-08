f9ee06e170618b084495c295e5adfe4b09d718ee
pratiktiwari001 <tiwaripratik222@gmail.com>
2026-07-08 16:18:33 +0530

    deploying

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
 export const BASE_URL = 
-  typeof window !== 'undefined' && window.location.hostname === "localhost" 
-    ? "https://dev-tinder-backend-kappa.vercel.app/" 
-    : "/api"; // <--- THIS IS THE KEY
+"https://dev-tinder-backend-kappa.vercel.app"
